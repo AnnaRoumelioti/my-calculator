53b0d878b87fae76e5237507c535cdfd4618a92d
AnnaRoumelioti <mrs.adelaida@gmail.com>
2025-01-13 07:13:27 +0200

    project created

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+
+public class AddCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 20;
+        int result = 0;
+    }
+
+        public static int add(int a, int b) {
+        return a + b;
+        }
+}
